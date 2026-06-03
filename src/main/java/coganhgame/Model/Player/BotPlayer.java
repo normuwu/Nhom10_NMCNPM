03557dfe9f4435103305af56f805a25f52ac7ca5
vanvochidat <23130058@st.hcmuaf.edu.vn>
2026-06-03 20:52:29 +0700

    tao botplayer

---
+++ src/main/java/coganhgame/Model/Player/BotPlayer.java
@@ -0,0 +1,8 @@
+package coganhgame.Model.Player;
+
+import coganhgame.Utilities.Constants;
+import coganhgame.Model.Game.GameWithBot;
+import coganhgame.Model.Move.Move;
+import coganhgame.Model.Move.MoveResult;
+import coganhgame.Model.Piece;
+import coganhgame.Model.Tile.Tile;
