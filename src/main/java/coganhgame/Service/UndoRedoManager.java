09d0ac64f4a363f737ae6630d017c4505174e7f8
Ngân <23130204@st.hcmuaf.edu.vn>
2026-06-04 23:38:04 +0700

    Thêm stack và MAX_UNDO

--- src/main/java/coganhgame/Service/UndoRedoManager.java
+++ src/main/java/coganhgame/Service/UndoRedoManager.java
@@ -1,1 +1,27 @@
+package coganhgame.Service;
+
+public class UndoRedoManager {
+
+
+  
+package coganhgame.Service;
+
+import coganhgame.Model.Game.UndoSnapshot;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class UndoRedoManager {
+
+    private static final int MAX_UNDO = 50;
+
+    private final Deque<UndoSnapshot> undoStack = new ArrayDeque<>();
+    private final Deque<UndoSnapshot> redoStack = new ArrayDeque<>();
+
+}
+
+  
+}
+
+
+
 
